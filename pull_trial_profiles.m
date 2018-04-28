320de50e8b56150f38244613c8df3a0a5f267930
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2018-04-28 19:14:12 +0100

    sync

--- pull_trial_profiles.m
+++ pull_trial_profiles.m
@@ -13,11 +13,16 @@ function [ E ] = pull_trial_profiles(EEG, TrialInfo, window, baseline, varargin)
 %  experiment, which are repeated in a different order in each session.
 %
 %  window : a 2 element vector which has the window onset and window
-%  duration.
+%  duration in milliseconds.
 %
-%  baseline : a scalar value that indicated how many time point prior to
+%  baseline : a scalar value that indicates how many milliseconds prior to
 %  stimulus onset should be treated as the baseline window. The mean of the
 %  baseline window will be subtracted from the trial activation.
+%
+%  boxcar : a scalar value that indicates a width (in milliseconds) of a
+%  boxcar defined to downsample without convolution. The timeseries for a
+%  trial will be broken into N contiguous pieces of size "boxcar", and the
+%  data within each boxcar is then averaged over time.
 %
 %  electrodes : If a list of electrode labels are provided, then only these
 %  electrodes will be returned in the output.
@@ -27,23 +32,33 @@ function [ E ] = pull_trial_profiles(EEG, TrialInfo, window, baseline, varargin)
 %  array. Trials are ordered by stimulus index, not order of presentation
 %  (so trial 1 in each session can be interpretted as the same stimulus.)
 %
-% Chris Cox 01 March 2018
+% Chris Cox 25 March 2018
 
     p = inputParser();
     addRequired(p, 'EEG', @isstruct);
     addRequired(p, 'TrialInfo', @istable);
     addRequired(p, 'window', @isvector);
     addRequired(p, 'baseline', @isscalar);
+    addOptional(p, 'boxcar', 0, @isscalar);
     addOptional(p, 'electrodes', cellstr(EEG.DIM(2).label), @iscellstr);
-    addParameter(p, 'ReturnBaseline', true, @islogical);
+    addParameter(p, 'ReturnBaseline', false, @islogical);
     parse(p, EEG, TrialInfo, window, baseline, varargin{:});
+    
+    Hz = 1 / EEG.DIM(1).interval; % ticks per second
+    boxcar_size   = (p.Results.boxcar / 1000) * Hz;
+    window_start  = (window(1) / 1000) * Hz;
+    window_size   = (window(2) / 1000) * Hz; % in ticks (where a tick is a single time-step).
+    baseline_size = (baseline  / 1000) * Hz; % in ticks (where a tick is a single time-step).
     
     allelectrodes = cellstr(EEG.DIM(2).label);
     sessions = sort(unique(TrialInfo.Session));
     E = struct('label',p.Results.electrodes,'data',[]);
     for k = 1:numel(p.Results.electrodes);
         currentElectrodeFilter = strcmp(p.Results.electrodes{k}, allelectrodes);
-        X = zeros(4, max(TrialInfo.Trial), window(2));
+        if ~any(currentElectrodeFilter)
+            continue;
+        end
+        X = zeros(4, max(TrialInfo.Trial), window_size);
         B = zeros(4, max(TrialInfo.Trial), baseline);
         B_raw = zeros(4, max(TrialInfo.Trial), baseline);
         for i = 1:numel(sessions)
@@ -55,26 +70,49 @@ function [ E ] = pull_trial_profiles(EEG, TrialInfo, window, baseline, varargin)
                 onset = SessionInfo.OnsetIndex(z);
                 stimid = SessionInfo.ItemIndex(z);
                 if baseline > 0;
-                    a = onset - baseline;
+                    a = onset - baseline_size;
                     b = onset - 1;
                     baseline_mean = mean(EEG.DATA(a:b,currentElectrodeFilter));
-                    B(session,stimid,:) = EEG.DATA(a:b,currentElectrodeFilter);
+                    B(session,stimid,:) = EEG.DATA(a:b,currentElectrodeFilter) - baseline_mean;
                     B_raw(session,stimid,:) = EEG.DATA(a:b,currentElectrodeFilter);
                 else
                     baseline_mean = 0;
                 end
-                a = onset + window(1);
-                b = a + window(2) - 1;
-%                 a = onset - 200;
-%                 b = onset + 1000;
+                a = onset + window_start;
+                b = a + window_size - 1;
                 X(session,stimid,:) = EEG.DATA(a:b,currentElectrodeFilter) - baseline_mean;
             end
         end
-        E(k).data = X;
+        E(k).data = boxcar_average_timepoints(X, boxcar_size);
         if p.Results.ReturnBaseline
-            E(k).baseline = B;
-            E(k).baseline_raw = B_raw;
+            E(k).baseline = boxcar_average_timepoints(B, boxcar_size);
+            E(k).baseline_raw = boxcar_average_timepoints(B_raw, boxcar_size);
         end
     end
 end
 
+function Xbc = boxcar_average_timepoints(X, boxcar_size)
+% BOXCAR_AVERAGE_TIMEPOINTS Downsample without convolution
+% 
+% Algorithm :
+% 1. Trim the window so that it is evenly divisible by the boxcar.
+% 2. Transpose the data to make it time by item.
+% 3. Reshape to be boxcar_size by adj_window_size / boxcar_size by item.
+% 4. Take mean over first dimension (default of mean()).
+% 5. Squeeze out the now singleton first dimension
+% 6. Transpose back to item by time.
+    if boxcar_size == 0
+        Xbc = X;
+    else
+        [nsessions, nitems, window_size] = size(X);
+        a = 1;
+        b = window_size - rem(window_size, boxcar_size);
+        c = b / boxcar_size;
+        Xbc = nan(nsessions, nitems, c);
+        for i = 1:nsessions
+            x = squeeze(X(i,:,a:b));
+            r = size(x, 1);
+            Xbc(i,:,:) = squeeze(mean(reshape(x',boxcar_size,c,r)))';
+        end
+    end
+end
